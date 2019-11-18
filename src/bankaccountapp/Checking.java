4370f0196b9b6ef0aefdcdad832167e97e433eec
hristog121 <hristog121@kth.se>
2019-11-18 18:48:59 +0100

    Card number generator + Pin generator

--- src/bankaccountapp/Checking.java
+++ src/bankaccountapp/Checking.java
@@ -9,15 +9,25 @@ public class Checking extends Account {
     public Checking(String name, String sSN, double initDeposit) {
         super(name, sSN,initDeposit);
         accountNumber = "2" + accountNumber;
+        setDebitCard();
 
     }
 
 
     // List any methods specific to the checking account
+    private void setDebitCard(){
+        debitCardNumber = (int) (Math.random() * Math.pow(10,12));
+        debitCardPIN = (int) (Math.random() * Math.pow(10,4));
 
+    }
     @Override
     public void showInfo() {
         super.showInfo();
-        System.out.println("ACCOUNT TYPE: Checking");
+        System.out.println(
+                "Your Debit Account Features" +
+                        "\n Debit Card Number: " + debitCardNumber +
+                        "\n Debit Card PIN: " + debitCardPIN
+
+        );
     }
 }
--- src/bankaccountapp/Savings.java
+++ src/bankaccountapp/Savings.java
@@ -4,11 +4,18 @@ public class Savings extends Account {
     // List props specific for the savings account
     int saftyDepositBoxID;
     int saftyDepositBoxKey;
+
     // Constructor to init settings for the Saving props
 
     public Savings(String name, String sSN, double initDeposit) {
         super(name, sSN,initDeposit);
         accountNumber = "1" + accountNumber;
+        setSafetyDepositBox();
+    }
+
+    private void setSafetyDepositBox(){
+        saftyDepositBoxID = (int) (Math.random() * Math.pow(10,3));
+        saftyDepositBoxKey = (int) (Math.random() * Math.pow(10,4));
     }
 
     // List methods specific for the saving accounts
@@ -16,7 +23,12 @@ public class Savings extends Account {
     @Override
     public void showInfo() {
         super.showInfo();
-        System.out.println("ACCOUNT TYPE: Savings");
+        System.out.println(
+                "Your Savings Account Features" +
+                        "\n Safety Deposit box ID: " + saftyDepositBoxID +
+                        "\n Safety Deposit box Key: " + saftyDepositBoxKey
+
+        );
 
     }
 }
